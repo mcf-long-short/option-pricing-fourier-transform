180bf1b8499cf8d74a6ccd3d6865268d8ae4d892
krivi95 <krivi95@hotmail.com>
2021-09-12 14:19:52 +0200

    add new models

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ class OPTION_PRICING_MODEL(Enum):
     BSM = 'BSM'
     BSM_FT_NUM = 'BSM via Furier Transform (Lewis)'
     BSM_FFT = 'BSM via FFT (Lewis)'
+    MERTON_FT_NUM = 'Merton via Fourier Transform (Lewis)'
+    MERTON_FFT = 'Merton via FFT (Carr-Madan)'
 
 
 @st.cache
@@ -19,9 +21,9 @@ def get_historical_data(ticker):
     return Ticker.get_historical_data(ticker)
 
 
-###################
-##Streamlit config#
-###################
+####################
+##Streamlit config##
+####################
 
 st.title('Option pricing')
 
@@ -31,6 +33,7 @@ pricing_method = st.sidebar.radio('Please select option pricing method', options
 # Displaying specified model
 st.subheader(f'Pricing method: {pricing_method}')
 
+
 # Model parameters
 # Parameters for Black-Scholes model
 ticker = st.text_input('Ticker symbol', 'AAPL')
@@ -39,10 +42,19 @@ risk_free_rate = st.slider('Risk-free rate (%)', 0, 100, 10)
 sigma = st.slider('Sigma (%)', 0, 100, 20)
 exercise_date = st.date_input('Exercise date', min_value=datetime.today() + timedelta(days=1), value=datetime.today() + timedelta(days=365))  # noqa
 
+# Additional parameters for jump-diffusion model (Merton model)
+if pricing_method == OPTION_PRICING_MODEL.MERTON_FFT.value or pricing_method == OPTION_PRICING_MODEL.MERTON_FT_NUM.value:
+    st.text("Parameters for jump-diffusion Merton model:")
+    lamb = st.number_input('Jump frequency', 1.)
+    mu = st.number_input('Expected jump size', -0.1)
+    delta = st.number_input('Jump size volatility', 0.1)
 
 if st.button(f'Calculate option price for {ticker}'):
     # Getting data for selected ticker
     data = get_historical_data(ticker)
+    if data is None:
+        st.warning(f"Couldn't get price for {ticker}.")
+        st.stop()
     print(data)
     st.write(data.tail())
     ticker_plot_pbj = Ticker.plot_data(data, ticker, 'Adj Close')
@@ -65,6 +77,10 @@ if st.button(f'Calculate option price for {ticker}'):
         call_price = option.price('call', 'BSM_FFT')
     elif pricing_method == OPTION_PRICING_MODEL.BSM_FT_NUM.value:
         call_price = option.price('call', 'BSM_FT_NUM')
+    elif pricing_method == OPTION_PRICING_MODEL.MERTON_FT_NUM.value:
+        call_price = option.price('call', 'MERTON_FT_NUM', lamb, mu, delta)
+    elif pricing_method == OPTION_PRICING_MODEL.MERTON_FFT.value:
+        call_price = option.price('call', 'MERTON_FFT', lamb, mu, delta)
 
     # Displaying call/put option price
     st.subheader(f'Call option price: {call_price}')
